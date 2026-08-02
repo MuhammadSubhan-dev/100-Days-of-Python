a2b324c1f22f1fff99bbca2acb4f90f0e97a5e3e
SubhanNU <k250714@nu.edu.pk>
2026-08-02 13:59:38 +0500

    Day 5: Password Generator

---
+++ Day-005-Password-Generator/main.py
@@ -0,0 +1,35 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Easy Level
+password = ""
+for letter in range(0,nr_letters):
+    password += random.choice(letters)
+for symbol in range(0,nr_symbols):
+    password += random.choice(symbols)
+for number in range(0,nr_numbers):
+    password += random.choice(numbers)
+print("Your Easy password is: ", password)
+
+#Hard Level
+Password =[]
+for letter in range(0,nr_letters):
+    Password.append(random.choice(letters))
+for symbol in range(0,nr_symbols):
+    Password.append(random.choice(symbols))
+for number in range(0,nr_numbers):
+    Password.append(random.choice(numbers))
+print(Password)
+random.shuffle(Password)    #This shuffles the characters in random order
+password_hard = ""
+for char in Password:
+    password_hard += char
+print("Your Hard password is: ", password_hard)
